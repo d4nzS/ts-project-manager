6fe715a3c6f0be3fc68dfefe782668705f96fd3d
d4nzS <dzianis.karym@gmail.com>
2022-12-29 15:01:26 +0300

    validate inputs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,39 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+
+  if (validatableInput.required) {
+    isValid = isValid && !!validatableInput.value.toString().trim();
+  }
+
+  if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLength;
+  }
+
+  if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length <= validatableInput.maxLength;
+  }
+
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+
+  return isValid;
+}
+
 // AutoBind decorator
 function AutoBind(
   _: any,
@@ -15,9 +51,7 @@ function AutoBind(
 class ProjectInput {
   templateElement = document.getElementById('project-input') as HTMLTemplateElement;
   hostElement = document.getElementById('app') as HTMLElement;
-  element = document
-    .importNode(this.templateElement.content, true)
-    .firstElementChild as HTMLFormElement;
+  element = document.importNode(this.templateElement.content, true).firstElementChild as HTMLFormElement;
   titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
   descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
   peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
@@ -34,10 +68,25 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    const titleValidatable: Validatable = {
+      value: this.titleInputElement.value,
+      required: true
+    };
+    const descriptionValidatable: Validatable = {
+      value: this.descriptionInputElement.value,
+      required: true
+    };
+    const peopleValidatable: Validatable = {
+      value: +this.peopleInputElement.value,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
     if (
-      !enteredTitle.trim() ||
-      !enteredDescription.trim() ||
-      !enteredPeople.trim()
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
       alert('Invalid input, please try again');
 
