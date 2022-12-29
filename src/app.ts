c8e7f37c6c1d44369f80e0c8a5ec8b4a13677141
d4nzS <dzianis.karym@gmail.com>
2022-12-29 14:09:49 +0300

    interact with dom

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,23 @@ class ProjectInput {
   element = document
     .importNode(this.templateElement.content, true)
     .firstElementChild as HTMLFormElement;
+  titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+  descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+  peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
 
   constructor() {
+    this.element.id = 'user-input';
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+  }
+
+  private configure(): void {
+    this.element.onsubmit = this.submitHandler.bind(this);
   }
 
   private attach(): void {
